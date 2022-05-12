879f96525b7f12e44c6a20336129b3155ebe897e
Simon Vonhoff <simon.vonhoff@outlook.com>
2022-05-12 23:32:03 +0200

    Added definition validation

--- ResourcePacker/Helpers/DefinitionHelper.cs
+++ ResourcePacker/Helpers/DefinitionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using Force.Crc32;
 using Serilog;
 
@@ -6,6 +7,9 @@ namespace ResourcePacker.Helpers
 {
     internal static class DefinitionHelper
     {
+        private static readonly Regex ValidDefinitionRegex = 
+            new(@"^((\.\./|[a-zA-Z0-9_/\-\\ ])*\.[a-zA-Z0-9]+)$", RegexOptions.Compiled);
+
         public static Dictionary<uint, string> Create(Stream fileStream)
         {
             var definitions = new Dictionary<uint, string>();
@@ -18,8 +22,14 @@ namespace ResourcePacker.Helpers
                 {
                     continue;
                 }
+                
+                value = value.Replace(@"\", "/").Trim().ToLowerInvariant();
+                if (!ValidDefinitionRegex.IsMatch(value))
+                {
+                    Log.Error("Invalid definition: {entry}", value);
+                    continue;
+                }
 
-                value = value.Replace(@"\", "/").ToLowerInvariant();
                 var bytes = Encoding.ASCII.GetBytes(value);
                 var crc = Crc32Algorithm.Compute(bytes);
 
@@ -31,6 +41,48 @@ namespace ResourcePacker.Helpers
                 }
 
                 Log.Warning("Duplicate definition: {entry}", value);
+            }
+
+            return definitions;
+        }
+
+        public static List<string>? LoadDefinitions(Stream fileStream)
+        {
+            var definitions = new List<string>();
+            var invalidDefinitionAmount = 0;
+            using var reader = new StreamReader(fileStream);
+            while (!reader.EndOfStream)
+            {
+                var value = reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    continue;
+                }
+
+                value = value.Replace(@"\", "/").Trim().ToLowerInvariant();
+                if (!ValidDefinitionRegex.IsMatch(value))
+                {
+                    Log.Error("Invalid definition: {entry}", value);
+                    invalidDefinitionAmount++;
+                    continue;
+                }
+
+                if (definitions.Contains(value))
+                {
+                    Log.Warning("Duplicate definition: {entry}", value);
+                    continue;
+                }
+
+                Log.Debug("Added definition: {entry}", value);
+                definitions.Add(value);
+            }
+
+            if (definitions.Count == 0)
+            {
+                MessageBox.Show("The specified file does not contain any valid file definitions. \r\n" +
+                                "A definition file should only contain the file paths of the files contained in the package.", 
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
             }
 
             return definitions;
