d086d03be5500195a3a608dd5276ad27b09dec80
Simon Vonhoff <simon.vonhoff@outlook.com>
2022-06-23 13:03:00 +0200

    Added service configuration

---
+++ src/Application/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ResourcePackerGUI.Application
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using ResourcePackerGUI.Application.Common.Interfaces;
+using ResourcePackerGUI.Infrastructure.Services;
+
+namespace ResourcePackerGUI.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IAesEncryptionService, AesEncryptionService>();
+            services.AddSingleton<ICrc32Service, Crc32Service>();
+            services.AddSingleton<IMediaTypeService, MediaTypeService>();
+            services.AddSingleton<IFileSystem, FileSystem>();
+            return services;
+        }
+    }
+}
