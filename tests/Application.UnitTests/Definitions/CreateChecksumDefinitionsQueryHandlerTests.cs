7296a5b004015884d9c11b853ac15a3cee7b8d1d
Simon Vonhoff <simon.vonhoff@outlook.com>
2022-06-23 15:03:04 +0200

    Added additional unit tests

--- tests/Application.UnitTests/Definitions/CreateChecksumDefinitionsQueryHandlerTests.cs
+++ tests/Application.UnitTests/Definitions/CreateChecksumDefinitionsQueryHandlerTests.cs
@@ -23,13 +23,34 @@ namespace Application.UnitTests.Definitions
         [Fact]
         public async Task CreateChecksumDefinitions()
         {
-            using var stream = new MemoryStream(Encoding.UTF8.GetBytes("ForeSt\\maP.TmX\nsmW/smw_FoReGround.TSX   "));
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes("forest/map.tmx\nsmw/smw_foreground.tsx"));
             var query = new CreateChecksumDefinitionsQuery(stream);
             var sut = new CreateChecksumDefinitionsQueryHandler(_crc32Serivce, _logger);
             var result = await sut.Handle(query, default);
             Assert.True(result?.TryGetValue(3090263786, out _) == true);
             Assert.True(result?.TryGetValue(3106401826, out _) == true);
-            Assert.True(result?.TryGetValue(3801234820, out _) == false);
+        }
+
+        [Fact]
+        public async Task CreateChecksumDefinitions_ShouldSkipDuplicates()
+        {
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes("sonic/base.tmx\nsonic/base.tmx\nsonic/base.tmx"));
+            var query = new CreateChecksumDefinitionsQuery(stream);
+            var sut = new CreateChecksumDefinitionsQueryHandler(_crc32Serivce, _logger);
+            var result = await sut.Handle(query, default);
+            Assert.True(result?.Count == 1);
+        }
+
+        [Fact]
+        public async Task CreateChecksumDefinitions_NameNormalization()
+        {
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes("  \n Tf4\\heLLarm.TxT \n\n  RacEr/trEEs.pNg\n  \n"));
+            var query = new CreateChecksumDefinitionsQuery(stream);
+            var sut = new CreateChecksumDefinitionsQueryHandler(_crc32Serivce, _logger);
+            var result = await sut.Handle(query, default);
+            Assert.True(result?.Count == 2);
+            Assert.True(result?.TryGetValue(3411133111, out _) == true);
+            Assert.True(result?.TryGetValue(859863405, out _) == true);
         }
     }
 }
