c97171a4459ae69253eef485e2b478316f16383c
Simon Vonhoff <simon.vonhoff@outlook.com>
2022-06-26 08:54:16 +0200

    Added unit tests

---
+++ tests/Application.UnitTests/PathEntries/CreatePathEntriesQueryHandlerTests.cs
@@ -0,0 +1,80 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using System.Text;
+using Application.UnitTests.Common;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using ResourcePackerGUI.Application.PathEntries.Handlers;
+using ResourcePackerGUI.Application.PathEntries.Queries;
+using Xunit;
+
+namespace Application.UnitTests.PathEntries
+{
+
+    public class CreatePathEntriesQueryHandlerTests
+    {
+        private readonly ILogger<CreatePathEntriesQueryHandler> _logger;
+
+        public CreatePathEntriesQueryHandlerTests()
+        {
+            _logger = new NullLogger<CreatePathEntriesQueryHandler>();
+        }
+
+        [Fact]
+        public async Task CreatePathEntries_ValidFiles()
+        {
+            Dictionary<string, MockFileData> mockFiles = new()
+            {
+                { "F:\\repos\\ResourcePacker\\Debug\\assets\\base.png", new MockFileData(Array.Empty<byte>()) },
+                { "F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-red.png", new MockFileData(Array.Empty<byte>()) },
+            };
+
+            var fileSystem = new MockFileSystem(mockFiles, "F:\\repos\\ResourcePacker\\Debug\\");
+            var query = new CreatePathEntriesQuery(mockFiles.Keys.ToList(), 4);
+            var sut = new CreatePathEntriesQueryHandler(fileSystem, _logger);
+            var result = await sut.Handle(query, default);
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.Equal("assets/base.png", result[0].RelativePath);
+            Assert.Equal("assets/mushroom-red.png", result[1].RelativePath);
+            Assert.Equal("F:\\repos\\ResourcePacker\\Debug\\assets\\base.png", result[0].AbsolutePath);
+            Assert.Equal("F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-red.png", result[1].AbsolutePath);
+        }
+
+        [Fact]
+        public async Task CreatePathEntries_ShouldIgnoreInvalidFilePaths()
+        {
+            Dictionary<string, MockFileData> mockFiles = new()
+            {
+                { "F:\\repos\\assets\\base.png", new MockFileData(Array.Empty<byte>()) },
+                { "F:\\repos\\assets\\section.png", new MockFileData(Array.Empty<byte>()) },
+                { "F:\\repos\\ResourcePacker\\Debug\\assets\\base.png", new MockFileData(Array.Empty<byte>()) },
+                { "F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-red.png", new MockFileData(Array.Empty<byte>()) },
+            };
+
+            IReadOnlyList<string> absoluteFilePaths = new List<string>()
+            {
+                string.Empty,
+                " ",
+                "\r\n",
+                ":{}$%=;&",
+                "F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-red.png",
+                "F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-blue.png",
+                "F:\\repos\\assets\\base.png",
+                "F:\\repos\\assets\\section.png",
+                "F:\\repos\\ResourcePacker\\assets\\player.png",
+                "F:\\repos\\ResourcePacker\\Debug\\assets\\base.png"
+            };
+
+            var fileSystem = new MockFileSystem(mockFiles, "F:\\repos\\ResourcePacker\\Debug\\");
+            var query = new CreatePathEntriesQuery(absoluteFilePaths, 4);
+            var sut = new CreatePathEntriesQueryHandler(fileSystem, _logger);
+            var result = await sut.Handle(query, default);
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.Equal("assets/mushroom-red.png", result[0].RelativePath);
+            Assert.Equal("assets/base.png", result[1].RelativePath);
+            Assert.Equal("F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-red.png", result[0].AbsolutePath);
+            Assert.Equal("F:\\repos\\ResourcePacker\\Debug\\assets\\base.png", result[1].AbsolutePath);
+        }
+    }
+}
---
+++ tests/Application.UnitTests/PathEntries/ExportPathEntriesQueryHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using Application.UnitTests.Common;
+using ResourcePackerGUI.Application.Common.Exceptions;
+using ResourcePackerGUI.Application.PathEntries.Handlers;
+using ResourcePackerGUI.Application.PathEntries.Queries;
+using ResourcePackerGUI.Domain.Entities;
+using Xunit;
+
+namespace Application.UnitTests.PathEntries
+{
+    [Collection(QueryCollection.CollectionName)]
+    public class ExportPathEntriesQueryHandlerTests
+    {
+        private readonly List<PathEntry> _pathEntries = new()
+        {
+            new PathEntry("F:\\repos\\ResourcePacker\\Debug\\assets\\base.png", "assets/base.png"),
+            new PathEntry("F:\\repos\\ResourcePacker\\Debug\\assets\\mushroom-red.png", "assets/mushroom-red.png")
+        };
+
+        [Fact]
+        public async Task ExportPathEntries()
+        {
+            const string output = "F:\\repos\\ResourcePacker\\Debug\\assets\\export.txt";
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>(),
+                "F:\\repos\\ResourcePacker\\Debug\\assets\\");
+            var query = new ExportPathEntriesQuery(_pathEntries, output);
+            var sut = new ExportPathEntriesQueryHandler(fileSystem);
+            var expectedOutput =
+                "assets/base.png" + Environment.NewLine +
+                "assets/mushroom-red.png" + Environment.NewLine;
+
+            await sut.Handle(query, default);
+            Assert.True(fileSystem.FileExists(output));
+            Assert.Equal(expectedOutput, fileSystem.GetFile(output).TextContents);
+        }
+
+        [Fact]
+        public async Task ExportPathEntries_NoOutput_ThrowsInvalidDataException()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>(),
+                "F:\\repos\\ResourcePacker\\Debug\\assets\\");
+            var query = new ExportPathEntriesQuery(_pathEntries, string.Empty);
+            var sut = new ExportPathEntriesQueryHandler(fileSystem);
+            await Assert.ThrowsAsync<InvalidOutputException>(() => sut.Handle(query, default));
+        }
+    }
+}
