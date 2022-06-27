0c9a5bf9876e0fa00344830e858b7a83cc29fcef
Simon Vonhoff <simon.vonhoff@outlook.com>
2022-06-27 11:29:33 +0200

    Revert crc32 test export

--- src/Infrastructure/Services/Crc32Service.cs
+++ src/Infrastructure/Services/Crc32Service.cs
@@ -1,6 +1,5 @@
 ﻿using Force.Crc32;
 using ResourcePackerGUI.Application.Common.Interfaces;
-using ResourcePackerGUI.Infrastructure.Utilities;
 
 namespace ResourcePackerGUI.Infrastructure.Services
 {
@@ -8,22 +7,12 @@ namespace ResourcePackerGUI.Infrastructure.Services
     {
         public uint Compute(byte[] input, int offset, int length)
         {
-            var r = Crc32Algorithm.Compute(input, offset, length);
-
-            using var stream = File.Open("G:\\Predefined\\crcv.txt", FileMode.Append);
-            using StreamWriter writer = new(stream);
-            writer.WriteLine("{"+ FnvHash.Compute(input.Select(v => (byte)(v + offset + length)).ToArray()) + ", "+r+"}");
-            return r;
+            return Crc32Algorithm.Compute(input, offset, length);
         }
 
         public uint Compute(byte[] input)
         {
-            var r = Crc32Algorithm.Compute(input);
-
-            using var stream = File.Open("G:\\Predefined\\crc.txt", FileMode.Append);
-            using StreamWriter writer = new(stream);
-            writer.WriteLine("{" + FnvHash.Compute(input) + ", " + r + "}");
-            return r;
+            return Crc32Algorithm.Compute(input);
         }
     }
 }
